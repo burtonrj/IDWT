0c1ad0741b03a581f0f3eb5d12551e08225d4bd2
burtonrj <burtonrj@cardiff.ac.uk>
2020-06-23 12:05:14 +0100

    Updated the outcome docs

--- nosql/outcome.py
+++ nosql/outcome.py
@@ -10,18 +10,41 @@ class Outcome(mongoengine.Document):
     -----------
     patient_id: str, required
         Unique patient ID
+    component: str, optional
+        Even component (for further stratification of event types)
+    eventType: str, required
+        The type of event
+    eventDate: Date, required
+        Date of event
+    eventTime: float, optional
+        Time passed in minutes (relative to 0:00am event date)
+    covidStatus: str, (default="U")
+        COVID-19 status at time of event (options: "P", "N", "U")
+    death: int, (default=0)
+        1 = event corresponds to patient death, else 0
+    criticalCareAdmission, int (default=0)
+        1 = event corresponds to a critical care admission, else 0
+    source: str, optional
+        Source of event
+    sourceType: str, optional
+        Type of source
+    destination: str, optional
+        The patients destination as a result of the event
+    wimd: int, optional
+        Welsh deprivation score
     """
 
     patientId = mongoengine.StringField(required=True)
     component = mongoengine.StringField(required=False)
     eventType = mongoengine.StringField(required=True)
     eventDate = mongoengine.DateField(required=True)
-    covidStatus = mongoengine.StringField(required=True, default="U", choices=("Y", "N", "U"))
+    eventTime = mongoengine.FloatField(required=False)
+    covidStatus = mongoengine.StringField(required=True, default="U", choices=("P", "N", "U"))
     death = mongoengine.IntField(required=True, default=0, choices=(1, 0))
     criticalCareAdmission = mongoengine.IntField(required=True, default=0, choices=(1, 0))
     source = mongoengine.StringField(required=False)
     sourceType = mongoengine.StringField(required=False)
-    destination = mongoengine.StringField(required=True)
+    destination = mongoengine.StringField(required=False)
     wimd = mongoengine.IntField(required=False)
 
     meta = {
