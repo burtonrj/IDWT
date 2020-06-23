4cb3eddbd4226aa5457d59dd5051555134608f08
burtonrj <burtonrj@cardiff.ac.uk>
2020-06-23 19:32:27 +0100

    Refactored testResult to Measurment for better naming conventions. Created add_new_measurement method.

---
+++ nosql/measurement.py
@@ -0,0 +1,30 @@
+import mongoengine
+
+
+class Measurement(mongoengine.DynamicDocument):
+    patientId = mongoengine.StringField(required=True)
+    name = mongoengine.StringField(required=True)
+    date = mongoengine.DateField(required=False)
+    time = mongoengine.FloatField(required=False)
+    requestSource = mongoengine.StringField(required=False)
+    notes = mongoengine.StringField(required=False)
+    flags = mongoengine.ListField(required=False)
+
+    meta = {
+        "db_alias": "core",
+        "collection": "testResults",
+        "allow_inheritance": True
+    }
+
+
+class ContinuousMeasurement(Measurement):
+    result = mongoengine.FloatField(required=True)
+    refRange = mongoengine.ListField(required=False)
+
+
+class DiscreteMeasurement(Measurement):
+    result = mongoengine.StringField(required=True)
+
+
+class ComplexMeasurement(Measurement):
+    result = mongoengine.ListField(required=True)
--- nosql/patient.py
+++ nosql/patient.py
@@ -1,10 +1,17 @@
 import mongoengine
 import warnings
 from .outcome import Outcome
-from .test_result import TestResult
+from .measurement import Measurement, ComplexMeasurement, ContinuousMeasurement, DiscreteMeasurement
 from .critical_care import CriticalCare
 from utilities import parse_datetime
 from Levenshtein import distance as levenshtein_distance
+
+
+def _add_if_value(document, input_variables):
+    for name, value in input_variables:
+        if value is not None:
+            document[name] = value
+    return document
 
 
 class Comorbidity(mongoengine.DynamicDocument):
@@ -70,16 +77,16 @@ class Patient(mongoengine.Document):
         1 = patient died during admission, else 0
     critical_care_stay: int, default = 0
         1 = patient had a stay in ICU during admission, else 0
-    outcomeEvents: ReferenceField
+    outcomeEvents: List(ReferenceField)
         Reference to outcome events, reverse delete rule = Pull (if an outcome event is deleted, it will
         automatically be pulled from this list of references)
-    testResults: ReferenceField
+    measurements: List(ReferenceField)
         Reference to test results, reverse delete rule = Pull (if a test result is deleted, it will
         automatically be pulled from this list of references)
-    criticalCare: ReferenceField
+    criticalCare: List(ReferenceField)
         Reference to critical care record, reverse delete rule = Pull (if a critical care record is deleted, it will
         automatically be pulled from this list of references)
-    comorbidities: ReferenceField
+    comorbidities: List(ReferenceField)
         Reference to associated comorbidities, reverse delete rule = Pull (if a type of comorbidity is deleted, it will
         automatically be pulled from this list of references)
     """
@@ -91,7 +98,7 @@ class Patient(mongoengine.Document):
     died = mongoengine.IntField(required=True, default=0, choices=(1, 0))
     criticalCareStay = mongoengine.IntField(required=True, default=0, choices=(1, 0))
     outcomeEvents = mongoengine.ListField(mongoengine.ReferenceField(Outcome, reverse_delete_rule=4))
-    testResults = mongoengine.ListField(mongoengine.ReferenceField(TestResult, reverse_delete_rule=4))
+    measurements = mongoengine.ListField(mongoengine.ReferenceField(Measurement, reverse_delete_rule=4))
     criticalCare = mongoengine.ListField(mongoengine.ReferenceField(CriticalCare, reverse_delete_rule=4))
     comorbidities = mongoengine.ListField(mongoengine.ReferenceField(Comorbidity, reverse_delete_rule=4))
 
@@ -117,7 +124,7 @@ class Patient(mongoengine.Document):
         None
         """
         for references in [self.outcomeEvents,
-                           self.testResults,
+                           self.measurements,
                            self.criticalCare]:
             for doc in references:
                 doc.delete(signal_kwargs=signal_kwargs, **write_concern)
@@ -171,26 +178,91 @@ class Patient(mongoengine.Document):
         # Parse datetime and check validity (None for date if invalid)
         event_datetime = parse_datetime(event_datetime)
         if event_datetime.get("date") is None:
-            warnings.warn(f"Datetime parsed when trying to generate a new outcome event for {self.patient_id}"
-                          f"was invalid!")
+            raise ValueError(f"Datetime parsed when trying to generate a new outcome event for {self.patient_id}"
+                             f"was invalid!")
         # Create outcome document
-        new_outcome = Outcome(event_type=event_type.strip(),
-                              event_date=event_datetime.get("date"),
-                              covid_status=covid_status,
+        new_outcome = Outcome(patientId=self.patient_id,
+                              eventType=event_type.strip(),
+                              eventDate=event_datetime.get("date"),
+                              covidStatus=covid_status,
                               death=death,
-                              critical_care_admission=critical_care_admission,
+                              criticalCareAdmission=critical_care_admission,
                               **kwargs)
         # Populate with optional parameters if given
-        for name, value in [("component", component),
-                            ("source", source),
-                            ("sourceType", source_type),
-                            ("wimd", wimd),
-                            ("eventTime", event_datetime.get("time"))]:
-            if value is not None:
-                new_outcome[name] = value
-        new_outcome.save()
+        new_outcome = _add_if_value(new_outcome, [("component", component),
+                                                  ("source", source),
+                                                  ("sourceType", source_type),
+                                                  ("wimd", wimd),
+                                                  ("eventTime", event_datetime.get("time"))])
+        new_outcome = new_outcome.save()
         self.outcomeEvents.append(new_outcome)
         self.save()
 
-    def add_new
+    def add_new_measurement(self,
+                            result,
+                            result_type: str,
+                            name: str,
+                            result_datetime: str or None = None,
+                            request_source: str or None = None,
+                            result_split_char: str = " ",
+                            notes: str or None = None,
+                            flags: list or None = None,
+                            ref_range: list or None = None,
+                            **kwargs):
+        if result_datetime is not None:
+            result_datetime = result_datetime(result_datetime)
+            if result_datetime.get("date") is None:
+                raise ValueError(f"Datetime parsed when trying to generate a new measurement document for "
+                                 f"{self.patient_id} was invalid!")
+        if ref_range:
+            assert len(ref_range) == 2, "ref_range should be a list of length two, the first value is the lower " \
+                                        "threshold and the second the upper"
+        if result_type == "continuous":
+            new_result = ContinuousMeasurement(patientId=self.patient_id,
+                                               name=name,
+                                               result=float(result),
+                                               **kwargs)
+            new_result = _add_if_value(new_result, [("date", result_datetime.get("date")),
+                                                    ("time", result_datetime.get("time")),
+                                                    ("requestSource", request_source),
+                                                    ("notes", notes),
+                                                    ("flags", flags),
+                                                    ("ref_range", ref_range)])
+        elif result_type == "discrete":
+            new_result = DiscreteMeasurement(patientId=self.patient_id,
+                                             name=name,
+                                             result=str(result),
+                                             **kwargs)
+            new_result = _add_if_value(new_result, [("date", result_datetime.get("date")),
+                                                    ("time", result_datetime.get("time")),
+                                                    ("requestSource", request_source),
+                                                    ("notes", notes),
+                                                    ("flags", flags)])
+        elif result_type == "complex":
+            result = str(result).split(sep=result_split_char)
+            new_result = ComplexMeasurement(patientId=self.patient_id,
+                                            name=name,
+                                            result=result,
+                                            **kwargs)
+            new_result = _add_if_value(new_result, [("date", result_datetime.get("date")),
+                                                    ("time", result_datetime.get("time")),
+                                                    ("requestSource", request_source),
+                                                    ("notes", notes),
+                                                    ("flags", flags)])
+        else:
+            raise ValueError("result_type must be one of: 'complex', 'continuous, or 'discrete'")
+
+        new_result = new_result.save()
+        self.measurements.append(new_result)
+        self.save()
+
+    def get_result_by_type(self, requested_type: str):
+        if requested_type == "continuous":
+            return [x for x in self.measurements if type(x) == ContinuousMeasurement]
+        if requested_type == "discrete":
+            return [x for x in self.measurements if type(x) == DiscreteMeasurement]
+        if requested_type == "complex":
+            return [x for x in self.measurements if type(x) == ComplexMeasurement]
+        raise ValueError("request_type must be one of: 'continuous', 'discrete', or 'complex'")
+
 
