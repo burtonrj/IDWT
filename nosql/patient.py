05c0f5ec843c4b1a691a8bbbb44c20df03162b5a
burtonrj <burtonrj@cardiff.ac.uk>
2020-06-23 18:06:19 +0100

    Created the add_outcome_event method for Patient, complete with docs

--- nosql/patient.py
+++ nosql/patient.py
@@ -90,27 +90,100 @@ class Patient(mongoengine.Document):
     covid = mongoengine.StringField(required=True, default="U", choices=("Y", "N", "U"))
     died = mongoengine.IntField(required=True, default=0, choices=(1, 0))
     criticalCareStay = mongoengine.IntField(required=True, default=0, choices=(1, 0))
-    outcomeEvents = mongoengine.ReferenceField(Outcome, reverse_delete_rule=4)
-    testResults = mongoengine.ReferenceField(TestResult, reverse_delete_rule=4)
-    criticalCare = mongoengine.ReferenceField(CriticalCare, reverse_delete_rule=4)
-    comorbidities = mongoengine.ReferenceField(Comorbidity, reverse_delete_rule=4)
+    outcomeEvents = mongoengine.ListField(mongoengine.ReferenceField(Outcome, reverse_delete_rule=4))
+    testResults = mongoengine.ListField(mongoengine.ReferenceField(TestResult, reverse_delete_rule=4))
+    criticalCare = mongoengine.ListField(mongoengine.ReferenceField(CriticalCare, reverse_delete_rule=4))
+    comorbidities = mongoengine.ListField(mongoengine.ReferenceField(Comorbidity, reverse_delete_rule=4))
 
     meta = {
         "db_alias": "core",
         "collection": "patients"
     }
 
+    def delete(self, signal_kwargs=None, **write_concern):
+        """
+        Method override for parent delete method. Removes all unique referenced documents for patient prior to delete.
+
+        Parameters
+        ----------
+        signal_kwargs: dict, optional
+            kwargs dictionary to be passed to the signal calls.
+        write_concern
+             Extra keyword arguments are passed down which will be used as options for the resultant getLastError
+             command. For example, save(..., w: 2, fsync: True) will wait until at least two servers have recorded
+             the write and will force an fsync on the primary server
+        Returns
+        -------
+        None
+        """
+        for references in [self.outcomeEvents,
+                           self.testResults,
+                           self.criticalCare]:
+            for doc in references:
+                doc.delete(signal_kwargs=signal_kwargs, **write_concern)
+        super().delete(self=self,
+                       signal_kwargs=signal_kwargs,
+                       **write_concern)
+
     def add_new_outcome(self,
-                        type: str,
-                        datetime: str,
-                        component: str or None = None):
-        event_datetime = parse_datetime(datetime)
+                        event_type: str,
+                        event_datetime: str,
+                        covid_status: str = "U",
+                        death: int = 0,
+                        critical_care_admission: int = 0,
+                        component: str or None = None,
+                        source: str or None = None,
+                        source_type: str or None = None,
+                        wimd: int or None = None):
+        """
+        Add a new outcome event for patient.
+
+        Parameters
+        ----------
+        event_type: str, required
+            populates eventType in Outcome
+        event_datetime: str, required
+            Date or DateTime string. Can handle variable formats but requires that in date, day precedes month, which
+            precedes year (see utilities.parse_datetime for more details). Parsed datetime populates eventDate and
+            eventTime in Outcome.
+        covid_status: str, required, (default="U")
+            populates covidStatus in Outcome
+        death: int, required, (default=0)
+            populates death in Outcome
+        critical_care_admission: int, required, (default=0)
+            populates criticalCareAdmission in Outcome
+        component: str, optional
+            populates component in Outcome
+        source: str, optional
+            populates source in Outcome
+        source_type: str, optional
+            populates sourceType in Outcome
+        wimd: int, optional
+            populates wimd in Outcome
+
+        Returns
+        -------
+        None
+        """
+        event_datetime = parse_datetime(event_datetime)
         if event_datetime.get("date") is None:
             warnings.warn(f"Datetime parsed when trying to generate a new outcome event for {self.patient_id}"
                           f"was invalid!")
-        new_outcome = Outcome(event_type=type.strip(),
-                              )
-        if component:
-            new_outcome.component = component.strip()
+        new_outcome = Outcome(event_type=event_type.strip(),
+                              event_date=event_datetime.get("date"),
+                              covid_status=covid_status,
+                              death=death,
+                              critical_care_admission=critical_care_admission)
+        for name, value in [("component", component),
+                            ("source", source),
+                            ("sourceType", source_type),
+                            ("wimd", wimd),
+                            ("eventTime", event_datetime.get("time"))]:
+            if value is not None:
+                new_outcome[name] = value
+        new_outcome.save()
+        self.outcomeEvents.append(new_outcome)
+        self.save()
 
+    def add_new
 
