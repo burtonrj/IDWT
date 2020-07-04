f04cb89db898004f83e19d608d23a48fea60ca09
burtonrj <burtonrj@cardiff.ac.uk>
2020-07-04 12:00:00 +0100

    Add requirements to setup.py

--- ProjectBevan/setup.py
+++ ProjectBevan/setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='ProjectBevan',
     version='0.0.1',
     packages=['sql', 'nosql', 'tests'],
     package_dir={'': 'ProjectBevan'},
+    install_requires=requirements,
     url='https://github.com/burtonrj/IDWT',
     license='MIT',
     author='Ross Burton',
